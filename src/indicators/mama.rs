9d2f9cc166fc3699c158f71d8505bddcef8ee1e2
Daniel Liszka <liszkadp@alumni.vcu.edu>
2024-12-23 22:53:57 -0500

    linting

--- src/indicators/mama.rs
+++ src/indicators/mama.rs
@@ -215,7 +215,7 @@ pub fn calculate_mama(input: &MamaInput) -> Result<MamaOutput, Box<dyn Error>> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::indicators::{data_loader::read_candles_from_csv, mama};
+    use crate::indicators::data_loader::read_candles_from_csv;
 
     #[test]
     fn test_mama_accuracy_with_tolerance() {
