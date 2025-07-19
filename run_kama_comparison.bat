c459288204fd8f449f23961376414e3ab82b1f53
Daniel Liszka <liszkadp@alumni.vcu.edu>
2025-07-19 11:25:24 -0400

    deletedd

--- run_kama_comparison.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call .venv\Scripts\activate
-python compare_kama_performance.py
--- run_kama_tests.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Running KAMA Python tests...
-call .venv\Scripts\activate
-cd tests\python
-python -m pytest test_kama.py -v
-pause
